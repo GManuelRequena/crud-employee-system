b28fb378b128fad6ae695c52c03290e4be463011
Manuel Requena <48953482+GManuelRequena@users.noreply.github.com>
2023-06-03 18:54:41 -0300

    First commit

---
+++ src/main/java/com/manuel/backendcrudemployeesystem/BackendCrudEmployeeSystemApplication.java
@@ -0,0 +1,13 @@
+package com.manuel.backendcrudemployeesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCrudEmployeeSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCrudEmployeeSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manuel/backendcrudemployeesystem/BackendCrudEmployeeSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manuel.backendcrudemployeesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCrudEmployeeSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
