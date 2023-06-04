20c8ea6a39361def0a9e7fadeb000f8ea00a5d3e
Manuel Requena <48953482+GManuelRequena@users.noreply.github.com>
2023-06-04 12:53:27 -0300

    Basic Swagger config working

--- src/main/java/com/manuel/backendcrudemployeesystem/model/Employee.java
+++ src/main/java/com/manuel/backendcrudemployeesystem/model/Employee.java
@@ -1,10 +1,14 @@
 package com.manuel.backendcrudemployeesystem.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+//import jakarta.persistence.Column;
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.Id;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.time.LocalDate;
 import java.util.Date;
 
